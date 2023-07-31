8c87cda43e0f8a1118ecd9204fcd68cb19bc307f
Midan2047 <jeff112630@gmail.com>
2023-07-31 23:19:17 +0900

    불필요 util 삭제

--- presentation/src/main/java/com/ddodang/intervalmusicspeedchanger/presentation/util/ComposableFunctionUtil.kt
+++ presentation/src/main/java/com/ddodang/intervalmusicspeedchanger/presentation/util/ComposableFunctionUtil.kt
@@ -3,14 +3,11 @@ package com.ddodang.intervalmusicspeedchanger.presentation.util
 import android.graphics.BitmapFactory
 import android.media.MediaMetadataRetriever
 import androidx.annotation.DrawableRes
-import androidx.annotation.RawRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.imageResource
-import androidx.constraintlayout.compose.MotionScene
 import com.ddodang.intervalmusicspeedchanger.presentation.R
 
 @Composable
@@ -25,14 +22,3 @@ fun retrieveThumbnailFromFile(filePath: String, @DrawableRes defaultImageId: Int
     }.getOrNull()?.asImageBitmap() ?: ImageBitmap.imageResource(id = defaultImageId)
 }
 
-@Composable
-fun motionSceneResource(@RawRes rawRes: Int): MotionScene {
-    if (LocalInspectionMode.current) return MotionScene(content = "")
-    val context = LocalContext.current
-    return MotionScene(
-        content = context.resources
-            .openRawResource(rawRes)
-            .readBytes()
-            .decodeToString()
-    )
-}
